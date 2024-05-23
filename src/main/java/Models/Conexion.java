7875724fe109f41b2956acd61ed52d44f8a35eed
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-22 22:52:22 -0500

    CLASE CONEXION: metodo para conectar y desconectar a la BD ademas del patron singleton

--- src/main/java/Models/Conexion.java
+++ src/main/java/Models/Conexion.java
@@ -8,11 +8,47 @@ import java.sql.*;
  */
 public class Conexion {
 
+    private Conexion() {
+
+    }
+
     private static Connection conexion;
     private static Conexion intancia;
 
     private static final String URL = "jdbc:mysql://localhost:3307/bd_login";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "1234";
+
+    public Connection conectarBD() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            System.out.println("Conectado");
+            return conexion;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error el conectar: " + e.toString());
+        }
+        return conexion;
+    }
+
+    public void desconectaBD() throws SQLException {
+        try {
+            conexion.close();
+            System.out.println("Desconectado");
+        } catch (SQLException e) {
+            System.out.println("Error al desconectar: " + e.toString());
+            conexion.close();
+        } finally {
+            conexion.close();
+        }
+    }
+
+    //patron singleton
+    public static Conexion getInstancia() {
+        if (intancia == null) {
+            intancia = new Conexion();
+        }
+        return intancia;
+    }
 
 }
