1f990769e3c371a9b7f6a951f3df0bdf8e4a3eb8
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-23 00:06:16 -0500

    frm inicio: Modificacion del metodo para validar que nos campos no esten vacios

--- src/main/java/Models/Utelerias.java
+++ src/main/java/Models/Utelerias.java
@@ -11,11 +11,11 @@ public class Utelerias {
 
     //validacion de campos
     public static String validarCampos(JTextField txtUsuario, JPasswordField txtContrasenia) {
-        if (txtUsuario.getText().trim().isEmpty()) {
+        if (txtUsuario.getText().trim().isEmpty() || txtUsuario.getText().trim().equals("Ingrese su nombre de usuario")) {
             return "Usuario";
         }
-
-        if (String.valueOf(txtContrasenia.getText()).isEmpty()) {
+        
+        if (String.valueOf(txtContrasenia.getPassword()).isEmpty() || String.valueOf(txtContrasenia.getPassword()).equals("**********")) {
             return "Contraseña";
         }
         return "";
