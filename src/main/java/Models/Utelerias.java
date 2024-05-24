9ccd16c7d03f99b3f11eef46dedd7269af8f2b15
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-23 23:05:10 -0500

    Clase Utelerias: mejorando el metodo para validar los campos

--- src/main/java/Models/Utelerias.java
+++ src/main/java/Models/Utelerias.java
@@ -11,12 +11,14 @@ public class Utelerias {
 
     //validacion de campos
     public static String validarCampos(JTextField txtUsuario, JPasswordField txtContrasenia) {
-        if (txtUsuario.getText().trim().isEmpty() || txtUsuario.getText().trim().equals("Ingrese su nombre de usuario")) {
-            return "Usuario";
+        if ((txtUsuario.getText().trim().isEmpty() || txtUsuario.getText().trim().equals("Ingrese su nombre de usuario"))
+                && (String.valueOf(txtContrasenia.getPassword()).isEmpty() || String.valueOf(txtContrasenia.getPassword()).equals("**********"))) {
+            return "Ambos campos";
         }
-        
-        if (String.valueOf(txtContrasenia.getPassword()).isEmpty() || String.valueOf(txtContrasenia.getPassword()).equals("**********")) {
-            return "Contraseña";
+        if (txtUsuario.getText().trim().isEmpty() || txtUsuario.getText().trim().equals("Ingrese su nombre de usuario")) {
+            return " Campo Usuario";
+        } else if (String.valueOf(txtContrasenia.getPassword()).isEmpty() || String.valueOf(txtContrasenia.getPassword()).equals("**********")) {
+            return "Campo Contraseña";
         }
         return "";
     }
