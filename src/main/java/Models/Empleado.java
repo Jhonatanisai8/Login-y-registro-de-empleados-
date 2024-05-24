0b3ecbea50e0716adca012a6acf8ed8a2b9b1597
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-24 15:36:15 -0500

    CLASE EMPLEADO: agregación de constructores

--- src/main/java/Models/Empleado.java
+++ src/main/java/Models/Empleado.java
@@ -6,6 +6,7 @@ package Models;
  */
 public class Empleado {
 
+    private int id;
     private String nombre;
     private String appPaterno;
     private String appMaterno;
@@ -14,7 +15,40 @@ public class Empleado {
     private String sexo;
     private String estadoCivil;
     private int numHijos;
-    
-    
-    
+
+    //constructor donde nostros le vamos a establecer los atrubitos a traves de los set
+    public Empleado() {
+
+    }
+
+    //contructor que nos va ayudara a eliminar 
+    public Empleado(int id) {
+        this.id = id;
+    }
+
+    //constuctor que nos va a ayudara modificar
+    public Empleado(String nombre, String appPaterno, String appMaterno, double sueldoBase, String area, String sexo, String estadoCivil, int numHijos) {
+        this.nombre = nombre;
+        this.appPaterno = appPaterno;
+        this.appMaterno = appMaterno;
+        this.sueldoBase = sueldoBase;
+        this.area = area;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+        this.numHijos = numHijos;
+    }
+
+    //constructor que nos va ayudara eliminar
+    public Empleado(int id, String nombre, String appPaterno, String appMaterno, double sueldoBase, String area, String sexo, String estadoCivil, int numHijos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.appPaterno = appPaterno;
+        this.appMaterno = appMaterno;
+        this.sueldoBase = sueldoBase;
+        this.area = area;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+        this.numHijos = numHijos;
+    }
+
 }
