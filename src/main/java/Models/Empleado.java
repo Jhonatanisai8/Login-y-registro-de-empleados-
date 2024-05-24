7251a2ebfb1211ee64ff73b48333e2876c73cdc1
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-24 15:25:24 -0500

    creacion de la clase empleado con sus repectivos atributos por el momento

---
+++ src/main/java/Models/Empleado.java
@@ -0,0 +1,20 @@
+package Models;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Empleado {
+
+    private String nombre;
+    private String appPaterno;
+    private String appMaterno;
+    private double sueldoBase;
+    private String area;
+    private String sexo;
+    private String estadoCivil;
+    private int numHijos;
+    
+    
+    
+}
