e9ca9d4189c6f68049e70feda5e9f08f716a7340
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-24 21:28:23 -0500

    INTERFACE EmpleadoDaoImple: clase donde se van a implementar los datos ya definidos en la interface EmpleadoDao

---
+++ src/main/java/DaoImplementacion/EmpleadoDaoImple.java
@@ -0,0 +1,33 @@
+package DaoImplementacion;
+
+import Interfaces.EmpleadoDao;
+import Models.Empleado;
+import java.util.List;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class EmpleadoDaoImple implements EmpleadoDao {
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        
+    }
+
+    @Override
+    public int insertarEmpleado(Empleado empleado) {
+        return 0;
+    }
+
+    @Override
+    public int modificarDatosEmpleado(Empleado empleado) {
+        return 0;
+    }
+
+    @Override
+    public int eliminarEmpleado(Empleado empleado) {
+        return 0;
+    }
+
+}
