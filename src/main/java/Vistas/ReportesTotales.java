cd75c1171f37377c2689f4f26868661c775af4e1
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-29 11:22:13 -0500

    Nueva imagen para el boton generar reporte  en el formulario REPORTES TOTALES

--- src/main/java/Vistas/ReportesTotales.java
+++ src/main/java/Vistas/ReportesTotales.java
@@ -6,6 +6,7 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Color;
 import java.awt.HeadlessException;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -51,7 +52,8 @@ public class ReportesTotales extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblDatos = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
+        GenerarReporteBtn = new javax.swing.JPanel();
+        btnGenerarReporte = new javax.swing.JLabel();
 
         jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 20)); // NOI18N
         jLabel1.setText("Reportes de EmpleadosTotales:");
@@ -66,12 +68,36 @@ public class ReportesTotales extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(tblDatos);
 
-        jButton1.setText("Generar Reporte");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+        GenerarReporteBtn.setBackground(new java.awt.Color(2, 75, 115));
+
+        btnGenerarReporte.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        btnGenerarReporte.setForeground(new java.awt.Color(255, 255, 255));
+        btnGenerarReporte.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btnGenerarReporte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/NUEVO (3).png"))); // NOI18N
+        btnGenerarReporte.setText("Generar Reporte");
+        btnGenerarReporte.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnGenerarReporte.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnGenerarReporteMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnGenerarReporteMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnGenerarReporteMouseExited(evt);
             }
         });
+
+        javax.swing.GroupLayout GenerarReporteBtnLayout = new javax.swing.GroupLayout(GenerarReporteBtn);
+        GenerarReporteBtn.setLayout(GenerarReporteBtnLayout);
+        GenerarReporteBtnLayout.setHorizontalGroup(
+            GenerarReporteBtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(btnGenerarReporte, javax.swing.GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+        );
+        GenerarReporteBtnLayout.setVerticalGroup(
+            GenerarReporteBtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(btnGenerarReporte)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -81,8 +107,9 @@ public class ReportesTotales extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(200, 200, 200)
+                        .addComponent(GenerarReporteBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(390, 390, 390))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE)
                         .addGap(476, 476, 476))
@@ -96,13 +123,13 @@ public class ReportesTotales extends javax.swing.JPanel {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(GenerarReporteBtn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(70, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnGenerarReporteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGenerarReporteMouseClicked
 
         Document documento = new Document();
 
@@ -151,11 +178,20 @@ public class ReportesTotales extends javax.swing.JPanel {
         } catch (DocumentException | HeadlessException | FileNotFoundException e) {
             System.out.println("Error al crear pdf: " + e.toString());
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnGenerarReporteMouseClicked
+
+    private void btnGenerarReporteMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGenerarReporteMouseEntered
+        GenerarReporteBtn.setBackground(new Color(3, 105, 160));
+    }//GEN-LAST:event_btnGenerarReporteMouseEntered
+
+    private void btnGenerarReporteMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGenerarReporteMouseExited
+        GenerarReporteBtn.setBackground(new Color(2, 75, 115));
+    }//GEN-LAST:event_btnGenerarReporteMouseExited
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel GenerarReporteBtn;
+    private javax.swing.JLabel btnGenerarReporte;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblDatos;
